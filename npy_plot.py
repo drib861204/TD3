4baf40bd229926af145a1121065af74de8f0d908
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-01-29 19:26:38 +0800

    0129 run.py line289 +map_location=cpu; mislab faster(nice)

--- npy_plot.py
+++ npy_plot.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 data_1 = np.load('results/13/TD3_RWIP_0.npy')
-data_2 = np.load('results/18/TD3_RWIP_0.npy')
+data_2 = np.load('results/10/TD3_RWIP_0.npy')
 
-i = 14
+i = 9
+x_axis = np.linspace(i,np.shape(data_2)[0],101-i)
+print(np.shape(data_2)[0])
 
 #plt.plot(data_1[i:])
-plt.plot(data_2[i:])
+plt.plot(x_axis*5000,data_2[i:])
 plt.show()
