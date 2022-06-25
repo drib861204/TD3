f8acf3a27dfe309f7602bf128628004aeb14fd84
drib861204 <56364972+drib861204@users.noreply.github.com>
2022-06-25 12:41:26 +0800

    0624 17-21 plot.sh update

--- plot.sh
+++ plot.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 
-python3 plot_graph.py --trial 12
-python3 main.py -l 1 --trial 12 --seed 0
-python3 main.py -l 1 --trial 12 --seed 1
-python3 main.py -l 1 --trial 12 --seed 2
-python3 main.py -l 1 --trial 12 --seed 3
-python3 main.py -l 1 --trial 12 --seed 4
+python3 plot_graph.py --trial 17
+python3 main.py -l 1 --trial 17 --seed 0
+python3 main.py -l 1 --trial 17 --seed 1
+python3 main.py -l 1 --trial 17 --seed 2
+python3 main.py -l 1 --trial 17 --seed 3
+python3 main.py -l 1 --trial 17 --seed 4
 
-python3 plot_graph.py --trial 13
-python3 main.py -l 1 --trial 13 --seed 0
-python3 main.py -l 1 --trial 13 --seed 1
-python3 main.py -l 1 --trial 13 --seed 2
-python3 main.py -l 1 --trial 13 --seed 3
-python3 main.py -l 1 --trial 13 --seed 4
+python3 plot_graph.py --trial 18
+python3 main.py -l 1 --trial 18 --seed 0
+python3 main.py -l 1 --trial 18 --seed 1
+python3 main.py -l 1 --trial 18 --seed 2
+python3 main.py -l 1 --trial 18 --seed 3
+python3 main.py -l 1 --trial 18 --seed 4
 
-python3 plot_graph.py --trial 14
-python3 main.py -l 1 --trial 14 --seed 0
-python3 main.py -l 1 --trial 14 --seed 1
-python3 main.py -l 1 --trial 14 --seed 2
-python3 main.py -l 1 --trial 14 --seed 3
-python3 main.py -l 1 --trial 14 --seed 4
+python3 plot_graph.py --trial 19
+python3 main.py -l 1 --trial 19 --seed 0
+python3 main.py -l 1 --trial 19 --seed 1
+python3 main.py -l 1 --trial 19 --seed 2
+python3 main.py -l 1 --trial 19 --seed 3
+python3 main.py -l 1 --trial 19 --seed 4
 
-python3 plot_graph.py --trial 15
-python3 main.py -l 1 --trial 15 --seed 0
-python3 main.py -l 1 --trial 15 --seed 1
-python3 main.py -l 1 --trial 15 --seed 2
-python3 main.py -l 1 --trial 15 --seed 3
-python3 main.py -l 1 --trial 15 --seed 4
+python3 plot_graph.py --trial 20
+python3 main.py -l 1 --trial 20 --seed 0
+python3 main.py -l 1 --trial 20 --seed 1
+python3 main.py -l 1 --trial 20 --seed 2
+python3 main.py -l 1 --trial 20 --seed 3
+python3 main.py -l 1 --trial 20 --seed 4
 
-python3 plot_graph.py --trial 16
-python3 main.py -l 1 --trial 16 --seed 0
-python3 main.py -l 1 --trial 16 --seed 1
-python3 main.py -l 1 --trial 16 --seed 2
-python3 main.py -l 1 --trial 16 --seed 3
-python3 main.py -l 1 --trial 16 --seed 4
+python3 plot_graph.py --trial 21
+python3 main.py -l 1 --trial 21 --seed 0
+python3 main.py -l 1 --trial 21 --seed 1
+python3 main.py -l 1 --trial 21 --seed 2
+python3 main.py -l 1 --trial 21 --seed 3
+python3 main.py -l 1 --trial 21 --seed 4
